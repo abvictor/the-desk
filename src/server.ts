8905766ec076a1f54832be91b6c15e16e5933d73
abvictor <victor.amaralb@hotmail.com>
2025-10-09 18:51:10 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -36,6 +36,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(() => console.log(`Server running on port ${process.env.PORT}`))
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 export default app
